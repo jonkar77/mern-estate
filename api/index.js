3d147f4c76ba778b4ecb76657fbb1485628e4de9
OJ <onkarjoshi296@gmail.com>
2023-12-25 16:54:17 +0530

    created sign up page

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from "dotenv";
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
+import cors from 'cors';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -15,6 +16,7 @@ mongoose.connect(process.env.MONGO).then(() => {
 
 const app=express();
 app.use(express.json());
+app.use(cors());
 
 app.listen(3000, () => {
 
