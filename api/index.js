ac38af9b0463b556dbc2cc14770a24792397e700
OJ <onkarjoshi296@gmail.com>
2023-12-30 19:32:03 +0530

    create user update api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from "dotenv";
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
+import cookieParser from 'cookie-parser';
 import cors from 'cors';
 dotenv.config();
 
@@ -17,6 +18,8 @@ mongoose.connect(process.env.MONGO).then(() => {
 const app=express();
 app.use(express.json());
 app.use(cors());
+
+app.use(cookieParser());
 
 app.listen(3000, () => {
 
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,11 @@
 import Express from "express";
-import { test } from "../controllers/user.controller.js";
+import { test, updateUser } from "../controllers/user.controller.js";
+import { verifyToken } from "../utils/verifyUser.js";
 
 
 const router=Express.Router();
 router.get('/test', test
 );
+router.post('/update/:id', verifyToken, updateUser)
 
 export default router;
