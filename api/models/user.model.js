ba5054c7fdec76f1a3a35278a62094cd5a80ac22
OJ <onkarjoshi296@gmail.com>
2023-12-30 12:31:16 +0530

    image upload completed

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -10,11 +10,16 @@ const userSchema=new mongoose.Schema({
         type: String,
         required: true,
         unique: true,
-      },
+    },
     password: {
         type: String,
         required: true,
-    }
+    },
+    avatar: {
+        type: String,
+        // default: "https://static.vecteezy.com/system/resources/thumbnails/020/765/399/small/default-profile-account-unknown-icon-black-silhouette-free-vector.jpg"
+    },
+
 }, { timestamps: true }
 );
 
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,10 +1,12 @@
 import Express from 'express';
-import { signup } from '../controllers/auth.controller.js';
+import { google, signup } from '../controllers/auth.controller.js';
 import { signin } from '../controllers/auth.controller.js';
 
 
 const router = Express.Router();
 router.post("/signup", signup);
 router.post("/signin", signin);
+router.post('/google', google);
+
 
 export default router;
